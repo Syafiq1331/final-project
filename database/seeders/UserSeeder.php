858a4b718ed69f6c2d0477fa24ccaa4178c657c5
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-06-22 17:17:25 +0700

    add:user dummy data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('admin'),
+                'role' => 'admin',
+                'farm_place_id' => 1,
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmai.com',
+                'password' => bcrypt('user'),
+                'role' => 'user',
+                'farm_place_id' => 1,
+            ],
+        ];
+
+        foreach ($data as $key => $value) {
+            \App\Models\User::create($value);
+        }
     }
 }
