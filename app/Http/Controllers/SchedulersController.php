b8b3e7e14c96a67a85ee4cc99f0f14aaa86c501d
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-07-05 08:28:37 +0700

    add:controller, model & migrations for schedulers

---
+++ app/Http/Controllers/SchedulersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Schedulers;
+use Illuminate\Http\Request;
+
+class SchedulersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Schedulers $schedulers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Schedulers $schedulers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Schedulers $schedulers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Schedulers $schedulers)
+    {
+        //
+    }
+}
