85522c0511eca245c58b85e9d6d7f17d1a171905
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-07-05 08:22:00 +0700

    fix:change from lahanModel to lahanController

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,15 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\features\LahanController;
 use App\Http\Controllers\api\features\ProfileController;
 use App\Models\Lahan;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::apiResource('/lahan', Lahan::class);
+    Route::apiResource('/lahan', LahanController::class);
     Route::apiResource('/profile', ProfileController::class);
 });
