41e30b71d285f796bc3a7acc253371ddb177b068
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-07-06 19:03:20 +0700

    add:protected Hidden created_at & updated_at

--- app/Models/FarmPlace.php
+++ app/Models/FarmPlace.php
@@ -20,6 +20,8 @@ class FarmPlace extends Model
         'user_id',
     ];
 
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
