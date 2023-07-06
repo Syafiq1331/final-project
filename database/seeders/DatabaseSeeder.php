00612245d7ff194703cddbbfee26e233f5ebfafc
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-07-06 19:07:04 +0700

    add:LahanSeeder in Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             FarmPlaceSeeder::class,
             UserSeeder::class,
             JenisLahanSeeder::class,
+            LahanSeeder::class,
         ]);
     }
 }
--- database/seeders/LahanSeeder.php
+++ database/seeders/LahanSeeder.php
@@ -12,6 +12,20 @@ class LahanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'name' => 'Lahan 1',
+                'desc' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+                'image' => 'https://i.pinimg.com/originals/0a/0e/6a/0a0e6a0b2b5b5b0b0b0b0b0b0b0b0b0b.jpg',
+                'plantTime' => '2021-01-01',
+                'harvestTime' => '2021-01-01',
+                'farm_place_id' => 1,
+                'jenisLahan_id' => 1,
+            ],
+        ];
+
+        foreach ($data as $key => $value) {
+            \App\Models\Lahan::create($value);
+        }
     }
 }
