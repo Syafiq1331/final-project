79af897c0ee048be6c8242d95d8123bc40c50978
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-07-06 19:07:26 +0700

    add:these routes api

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\features\FieldController;
+use App\Http\Controllers\api\features\HomeController;
 use App\Http\Controllers\api\features\LahanController;
 use App\Http\Controllers\api\features\ProfileController;
-use App\Models\Lahan;
+use App\Http\Controllers\api\features\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/users', UserController::class);
     Route::apiResource('/lahan', LahanController::class);
     Route::apiResource('/profile', ProfileController::class);
+    Route::apiResource('/field', FieldController::class);
+    Route::apiResource('/dashboard', HomeController::class);
 });
